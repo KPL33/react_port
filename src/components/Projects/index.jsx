091f75fac6799a279e356cb631e9e330ecb1a308
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-25 15:14:44 -0700

    project boxes added

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -3,23 +3,33 @@ import './styles.css'
 export default function Projects() {
     return (
         <main>
-            <div className="work">
+            <div className="row1">
+                <div className="box">
+                
+                </div>
+                <div className="box">
+                
+                </div>
+
             </div>
 
-            <div className="work">
+            <div className="row2">
+                <div className="box">
+                
+                </div>
+                <div className="box">
+                
+                </div>
             </div>
 
-            <div className="work">
-            </div>
-
-            <div className="work">
-            </div>
-
-            <div className="work">
-            </div>
-
-            <div className="work">
-            </div>            
+            <div className="row3">
+                <div className="box">
+                
+                </div>
+                <div className="box">
+                
+                </div>
+            </div>           
         </main>
     );
 }
