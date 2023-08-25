cc93a8528a139da201ad2d54a3c21ffde09d5a9d
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-24 18:25:49 -0700

    resume bug fix

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,7 +1,9 @@
+import resume from "../../assets/kevin_lewis_Resume_2023.pdf"
+
 export default function Resume() {
     return (
       <div>
-        <iframe src="src/assets/kevin_lewis_Resume_2023.pdf" width="100%" height="600px" title="Resume"></iframe>
+        <iframe src={ resume } width="100%" height="600px" title="Resume"></iframe>
       </div>
     );
   }
