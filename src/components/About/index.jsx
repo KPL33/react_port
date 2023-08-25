8a12e71a4be9a46cf22aabc34ae08919299a6eaf
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-25 16:21:22 -0700

    About page styled

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,12 +5,16 @@ import './styles.css';
 export default function About() {
     return (
       <section id="about">
-        <img id="profile-pic" src={ profilepic } alt="Picture of Kevin Lewis" />
+        <div id="pic-box">
+          <img id="profile-pic" src={ profilepic } alt="Picture of Kevin Lewis" />
+        </div>
 
-        <p>
+        <div id="paragraph-box">
+          <p>
         I have over 23 years of experience in the industries of Design and Publishing. For the past 15+ years, I served as Production Manager for Publishers' Development Corporation. Due to budget cuts, my role was absorbed and I am looking for a new opportunity to contribute to the bright future of a stable company.
-        </p>
-       </section>
+          </p>
+        </div>   
+      </section>
     );
   }
   
