3f3a33ab42ced1b0e3f5eab0cada6ccd164839e9
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-26 13:41:33 -0700

    contact form added

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./styles.css";
+
 //This shows the contents of our "ContactPage".
 export default function Contact() {
-    return (
-      <section>
-        <p>
-        Contact Me:
-        </p>
-       </section>
-    );
-  }
+  return (
+    <section>
+      <form>
+      <h4>Contact</h4>
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="form4Example1">
+            <h6>Name:</h6>
+          </label>
+          <input type="text" id="form4Example1" className="form-control" />
+        </div>
+
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="form4Example2">
+          <h6>Email Address:</h6>
+          </label>
+          <input type="email" id="form4Example2" className="form-control" />
+        </div>
+
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="form4Example3">
+          <h6>Message:</h6>
+          </label>
+          <textarea
+            className="form-control"
+            id="form4Example3"
+            rows="4"
+          ></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block mb-4">
+          Send
+        </button>
+      </form>
+    </section>
+  );
+}
