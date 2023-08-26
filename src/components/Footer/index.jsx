e9235617503369a4b53669973a794047f49b994c
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-26 15:24:42 -0700

    Projects page developed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,12 +8,12 @@ export default function Footer() {
   return (
     <footer>
       <div id="socials">
-      <a href="https://www.linkedin.com/in/kevin-lewis92126"><img id="linkedin" src={ linkedin } alt="LinkedIn logo" /></a>
-      <a href="https://github.com/KPL33"><img id="github" src={ github }  alt="GitHub logo" /></a>
+        <a href="https://www.linkedin.com/in/kevin-lewis92126"><img id="linkedin" src={ linkedin } alt="LinkedIn logo" /></a>
+        <a href="https://gi thub.com/KPL33"><img id="github" src={ github }  alt="GitHub logo" /></a>
         <a href="https://leetcode.com/KPL33/"><img id="leetcode" src={ leetcode } alt="LeetCode logo" /></a>
       </div>
       <div>
-        <p>Made with React, by Kevin Lewis</p>
+        <p id="madewith">Made with React, by Kevin Lewis</p>
       </div>      
     </footer>
   );
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,35 +1,41 @@
 import './styles.css'
+import gitajob from '../../assets/gitajob.png';
+import mfc from '../../assets/mfc.png';
 
 export default function Projects() {
     return (
         <main>
-            <div className="row1">
-                <div className="box">
-                
-                </div>
-                <div className="box">
-                
-                </div>
-
+            <h3 className="intro">My Projects</h3>
+            <div className="projects">
+                <a href="https://shadowfre4k.github.io/Movie-Fight-Club/">
+                    <img className="box-container" src = { mfc } alt="Image of 'Movie Fight Club App'" />
+                </a>
+            
+                <a href="https://murmuring-sierra-21351-123e19ae1f23.herokuapp.com/login">
+                    <img className="box-container" src = { gitajob } alt="Image of 'GitAJob App'" />
+                    </a>
             </div>
 
-            <div className="row2">
-                <div className="box">
-                
-                </div>
-                <div className="box">
-                
-                </div>
+            <div className="projects">
+                <a href="https://shadowfre4k.github.io/Movie-Fight-Club/">
+                    <img className="box-container" src = { mfc } alt="Image of 'Movie Fight Club App'" />
+                </a>
+            
+                <a href="https://murmuring-sierra-21351-123e19ae1f23.herokuapp.com/login">
+                    <img className="box-container" src = { gitajob } alt="Image of 'GitAJob App'" />
+                    </a>
             </div>
-
-            <div className="row3">
-                <div className="box">
-                
-                </div>
-                <div className="box">
-                
-                </div>
-            </div>           
+                   
+            <div className="projects">
+                <a href="https://shadowfre4k.github.io/Movie-Fight-Club/">
+                    <img className="box-container" src = { mfc } alt="Image of 'Movie Fight Club App'" />
+                </a>
+            
+                <a href="https://murmuring-sierra-21351-123e19ae1f23.herokuapp.com/login">
+                    <img className="box-container" src = { gitajob } alt="Image of 'GitAJob App'" />
+                    </a>
+            </div>            
+                     
         </main>
     );
 }
