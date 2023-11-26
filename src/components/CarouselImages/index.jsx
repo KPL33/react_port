8e9c4d619fce23295b165818ee1746d681ff18b7
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-11-25 17:27:40 -0800

    Carousel test

--- src/components/CarouselImages/index.jsx
+++ src/components/CarouselImages/index.jsx
@@ -1,25 +1,25 @@
-import activa from "../../assets/activa.jpg"; 
-import ah from "../../assets/ah.jpg"; 
-import arted from "../../assets/arted.jpg"; 
+// import activa from "../../assets/activa.jpg"; 
+// import ah from "../../assets/ah.jpg"; 
+// import arted from "../../assets/arted.jpg"; 
 import battleCompPrev from "../../assets/battlecomp_prev.jpg"; 
-import checkmatePrev from "../../assets/checkmate_prev.jpg";
-import diamondOnePrev from "../../assets/diamond1_prev.jpg";
-import diamondTwoPrev from "../../assets/diamond2_prev.jpg";
-import diamondThreePrev from "../../assets/diamond3_prev.jpg";
-import fmglinkedin from "../../assets/fmglinkedin.jpg";
-import gemini from "../../assets/gemini.jpg";
-import gn from "../../assets/gn.jpg";
-import insider from "../../assets/insider.jpg";
-import merch from "../../assets/merch.jpg";
-import mauser from "../../assets/mauser.jpg";
-import ocken from "../../assets/ocken.jpg";
+// import checkmatePrev from "../../assets/checkmate_prev.jpg";
+// import diamondOnePrev from "../../assets/diamond1_prev.jpg";
+// import diamondTwoPrev from "../../assets/diamond2_prev.jpg";
+// import diamondThreePrev from "../../assets/diamond3_prev.jpg";
+// import fmglinkedin from "../../assets/fmglinkedin.jpg";
+// import gemini from "../../assets/gemini.jpg";
+// import gn from "../../assets/gn.jpg";
+// import insider from "../../assets/insider.jpg";
+// import merch from "../../assets/merch.jpg";
+// import mauser from "../../assets/mauser.jpg";
+// import ocken from "../../assets/ocken.jpg";
 
-import perfectmold from "../../assets/perfectmold.jpg";
-import righttool from "../../assets/righttool.jpg";
-import socialad from "../../assets/socialad.jpg";
+// import perfectmold from "../../assets/perfectmold.jpg";
+// import righttool from "../../assets/righttool.jpg";
+// import socialad from "../../assets/socialad.jpg";
 // import twitPrev from "../../assets/fmg_twitter_prev.jpg";
-import tyler from "../../assets/tyler.jpg";
-import warne from "../../assets/warne.jpg";
+// import tyler from "../../assets/tyler.jpg";
+// import warne from "../../assets/warne.jpg";
 
 const AdShowcase = [
   {
@@ -29,139 +29,139 @@ const AdShowcase = [
     alt: "First Image",
     src: battleCompPrev,
   },
-  {
-    id: 2,
-    title: "CheckMate - print ad",
-    description: "Image of CheckMate print ad.",
-    alt: "Second Image",
-    src: checkmatePrev,
-  },
-  {
-    id: 3,
-    title: "DiamondD Custom Leather - print ad, version 1",
-    description: "Image of DiamondD Custom Leather print ad, version 1",
-    alt: "Third Image",
-    src: diamondOnePrev,
-  },
-  {
-    id: 4,
-    title: "DiamondD Custom Leather - print ad, version 2",
-    description: "Image of DiamondD Custom Leather print ad, version 2",
-    alt: "Fourth Image",
-    src: diamondTwoPrev,
-  },
-  {
-    id: 5,
-    title: "DiamondD Custom Leather - print ad, version 3",
-    description: "Image of DiamondD Custom Leather print ad, version 3",
-    alt: "Fifth Image",
-    src: diamondThreePrev,
-  },
-  {
-    id: 6,
-    title: "FMG Holiday Gift - digital ad",
-    description: "Image of FMG Holiday Gift digital ad",
-    alt: "Sixth Image",
-    src: merch,
-  },
-  {
-    id: 7,
-    title: "Gemini Customs - digital ad",
-    description: "Image of Gemini Customs digital ad",
-    alt: "Seventh Image",
-    src: gemini,
-  },
-  {
-    id: 8,
-    title: "Tyler Gunworks - print ad",
-    description: "Image of Tyler Gunworks print ad",
-    alt: "Eighth Image",
-    src: tyler,
-  },
-  {
-    id: 9,
-    title: "Warne Tactical - print ad",
-    description: "Image of Warne Tactical print ad",
-    alt: "Ninth Image",
-    src: warne,
-  },
-  {
-    id: 10,
-    title: "Mitchell's Mausers - digital ad",
-    description: "Image of Mitchell's Mausers digital ad",
-    alt: "Tenth Image",
-    src: mauser,
-  },
-  {
-    id: 11,
-    title: "FMG LinkedIn - digital ad",
-    description: "Image of FMG LinkedIn digital ad",
-    alt: "Eleventh Image",
-    src: fmglinkedin,
-  },
-  {
-    id: 12,
-    title: "FMG Insider Tips - digital ad",
-    description: "Image of FMG Insider Tips digital ad",
-    alt: "Twelfth Image",
-    src: insider,
-  },
-  {
-    id: 13,
-    title: "Guns Magazine Website Promo - print ad",
-    description: "Image of Guns Magazine Website Promo print ad",
-    alt: "Thirteenth Image",
-    src: gn,
-  },
-  {
-    id: 14,
-    title: "American Handgunner Website Promo - print ad",
-    description: "Image of American Handgunner Website Promo print ad",
-    alt: "Fourteenth Image",
-    src: ah,
-  },
-  {
-    id: 15,
-    title: "FMG Social Media Promo - print ad",
-    description: "Image of FMG Social Media Promo print ad",
-    alt: "Fifteenth Image",
-    src: socialad,
-  },
-  {
-    id: 16,
-    title: "Art of Education - print ad",
-    description: "Image of Art of Education print ad",
-    alt: "Sixteenth Image",
-    src: arted,
-  },
-  {
-    id: 17,
-    title: "Perfect Mold - print ad",
-    description: "Image of Perfect Mold print ad",
-    alt: "Seventeenth Image",
-    src: perfectmold,
-  },
-  {
-    id: 18,
-    title: "Activa Products - print ad",
-    description: "Image of Activa Products print ad",
-    alt: "Eighteenth Image",
-    src: activa,
-  },
-  {
-    id: 19,
-    title: "The Right Tool - print ad",
-    description: "Image of The Right Tool print ad",
-    alt: "Nineteenth Image",
-    src: righttool,
-  },
-  {
-    id: 20,
-    title: "Ocken Acreage Bullet Catch - print ad",
-    description: "Ocken Acreage Bullet Catch print ad",
-    alt: "Twentieth Image",
-    src: ocken,
-  },
+  // {
+  //   id: 2,
+  //   title: "CheckMate - print ad",
+  //   description: "Image of CheckMate print ad.",
+  //   alt: "Second Image",
+  //   src: checkmatePrev,
+  // },
+  // {
+  //   id: 3,
+  //   title: "DiamondD Custom Leather - print ad, version 1",
+  //   description: "Image of DiamondD Custom Leather print ad, version 1",
+  //   alt: "Third Image",
+  //   src: diamondOnePrev,
+  // },
+  // {
+  //   id: 4,
+  //   title: "DiamondD Custom Leather - print ad, version 2",
+  //   description: "Image of DiamondD Custom Leather print ad, version 2",
+  //   alt: "Fourth Image",
+  //   src: diamondTwoPrev,
+  // },
+  // {
+  //   id: 5,
+  //   title: "DiamondD Custom Leather - print ad, version 3",
+  //   description: "Image of DiamondD Custom Leather print ad, version 3",
+  //   alt: "Fifth Image",
+  //   src: diamondThreePrev,
+  // },
+  // {
+  //   id: 6,
+  //   title: "FMG Holiday Gift - digital ad",
+  //   description: "Image of FMG Holiday Gift digital ad",
+  //   alt: "Sixth Image",
+  //   src: merch,
+  // },
+  // {
+  //   id: 7,
+  //   title: "Gemini Customs - digital ad",
+  //   description: "Image of Gemini Customs digital ad",
+  //   alt: "Seventh Image",
+  //   src: gemini,
+  // },
+  // {
+  //   id: 8,
+  //   title: "Tyler Gunworks - print ad",
+  //   description: "Image of Tyler Gunworks print ad",
+  //   alt: "Eighth Image",
+  //   src: tyler,
+  // },
+  // {
+  //   id: 9,
+  //   title: "Warne Tactical - print ad",
+  //   description: "Image of Warne Tactical print ad",
+  //   alt: "Ninth Image",
+  //   src: warne,
+  // },
+  // {
+  //   id: 10,
+  //   title: "Mitchell's Mausers - digital ad",
+  //   description: "Image of Mitchell's Mausers digital ad",
+  //   alt: "Tenth Image",
+  //   src: mauser,
+  // },
+  // {
+  //   id: 11,
+  //   title: "FMG LinkedIn - digital ad",
+  //   description: "Image of FMG LinkedIn digital ad",
+  //   alt: "Eleventh Image",
+  //   src: fmglinkedin,
+  // },
+  // {
+  //   id: 12,
+  //   title: "FMG Insider Tips - digital ad",
+  //   description: "Image of FMG Insider Tips digital ad",
+  //   alt: "Twelfth Image",
+  //   src: insider,
+  // },
+  // {
+  //   id: 13,
+  //   title: "Guns Magazine Website Promo - print ad",
+  //   description: "Image of Guns Magazine Website Promo print ad",
+  //   alt: "Thirteenth Image",
+  //   src: gn,
+  // },
+  // {
+  //   id: 14,
+  //   title: "American Handgunner Website Promo - print ad",
+  //   description: "Image of American Handgunner Website Promo print ad",
+  //   alt: "Fourteenth Image",
+  //   src: ah,
+  // },
+  // {
+  //   id: 15,
+  //   title: "FMG Social Media Promo - print ad",
+  //   description: "Image of FMG Social Media Promo print ad",
+  //   alt: "Fifteenth Image",
+  //   src: socialad,
+  // },
+  // {
+  //   id: 16,
+  //   title: "Art of Education - print ad",
+  //   description: "Image of Art of Education print ad",
+  //   alt: "Sixteenth Image",
+  //   src: arted,
+  // },
+  // {
+  //   id: 17,
+  //   title: "Perfect Mold - print ad",
+  //   description: "Image of Perfect Mold print ad",
+  //   alt: "Seventeenth Image",
+  //   src: perfectmold,
+  // },
+  // {
+  //   id: 18,
+  //   title: "Activa Products - print ad",
+  //   description: "Image of Activa Products print ad",
+  //   alt: "Eighteenth Image",
+  //   src: activa,
+  // },
+  // {
+  //   id: 19,
+  //   title: "The Right Tool - print ad",
+  //   description: "Image of The Right Tool print ad",
+  //   alt: "Nineteenth Image",
+  //   src: righttool,
+  // },
+  // {
+  //   id: 20,
+  //   title: "Ocken Acreage Bullet Catch - print ad",
+  //   description: "Ocken Acreage Bullet Catch print ad",
+  //   alt: "Twentieth Image",
+  //   src: ocken,
+  // },
 ];
 
 export default AdShowcase;
