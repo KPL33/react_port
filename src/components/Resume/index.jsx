f167536295c2a8a563ca442909caef4c071427e1
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-01-19 16:26:38 -0800

    added MS doc resume

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,10 +1,24 @@
 import "./styles.css";
-import resume from "../../assets/Kevin_Lewis_Dev_Resume_2024.png";
+import resume from "../../assets/Kevin_Lewis_Dev_Resume_1-24.png";
 
 export default function Resume() {
     return (
       <div className="resume">
         <img src={resume} />
+        <a
+          className="download"
+          href="../../assets/Kevin_Lewis_FS_resume_1-24.docx"
+          download="Kevin_Lewis_FS_resume.docx"
+        >
+          Download MS Word .docx
+        </a>
+        <a
+          className="download"
+          href="../../assets/Kevin_Lewis_Dev_Resume_1-24.pdf"
+          download="Kevin_Lewis_FS_resume.pdf"
+        >
+          Download .pdf
+        </a>
       </div>
     );
 }
