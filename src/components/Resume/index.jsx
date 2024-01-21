cec6ee4ee4c44ba495eedd7e2461964c73f7cfbf
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-01-20 16:50:27 -0800

    Resume download links working

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,24 +1,25 @@
 import "./styles.css";
-import resume from "../../assets/Kevin_Lewis_Dev_Resume_1-24.png";
+import resume from "../../assets/Kevin_Lewis_Dev_Resume_1-20-24.png";
+import pdfDoc from "../../assets/Kevin_Lewis_Dev_Resume_1-20-24.pdf";
 
 export default function Resume() {
-    return (
-      <div className="resume">
-        <img src={resume} />
-        <a
-          className="download"
-          href="../../assets/Kevin_Lewis_FS_resume_1-24.docx"
-          download="Kevin_Lewis_FS_resume.docx"
-        >
-          Download MS Word .docx
-        </a>
-        <a
-          className="download"
-          href="../../assets/Kevin_Lewis_Dev_Resume_1-24.pdf"
-          download="Kevin_Lewis_FS_resume.pdf"
-        >
-          Download .pdf
-        </a>
-      </div>
-    );
+  return (
+    <div className="resume">
+      <img src={resume} alt="Resume" />
+      <a
+        className="download"
+        href="/Kevin_Lewis_Dev_resume_1-20-24.docx" // Adjust the path as needed
+        download="Kevin_Lewis_FS_Resume.docx"
+      >
+        Download MS Word .docx
+      </a>
+      <a
+        className="download"
+        href={pdfDoc}
+        download="Kevin_Lewis_Dev_Resume.pdf"
+      >
+        Download .pdf
+      </a>
+    </div>
+  );
 }
