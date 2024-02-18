2a40de7e913480046ea5a0d9c5711fbebad930f8
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-02-18 10:05:40 -0800

    form 2 complete revert

--- src/components/Page/Contact2/index.jsx
+++ src/components/Page/Contact2/index.jsx
@@ -1,18 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 export default function Contact2() {
   return (
     <form name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
-      <h3 id="contact-title">Contact</h3>
-      <p className="contact-two">
+      <p>
         <label>
-          Your Name:<input type="text" name="name" />
+          Your Name: <input type="text" name="name" />
         </label>
       </p>
       <p>
         <label>
-          Your Email Address: <input type="email" name="email" />
+          Your Email: <input type="email" name="email" />
         </label>
       </p>
       <p>
