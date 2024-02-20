2096c6f52685f78021f44f6413c6406cfc524e6a
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-02-20 15:44:44 -0800

    more contact form debugging

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,17 +1,12 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 export default function Contact() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [isValidEmail, setIsValidEmail] = useState(true);
   const [message, setMessage] = useState("");
-
-  const messageRef = useRef(null);
-
-  useEffect(() => {
-    adjustTextareaHeight(messageRef.current);
-  }, [message]);
 
   const handleEmailChange = (event) => {
     const enteredEmail = event.target.value;
@@ -21,16 +16,6 @@ export default function Contact() {
     setIsValidEmail(emailPattern.test(enteredEmail));
   };
 
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-    adjustTextareaHeight(event.target);
-  };
-
-  const adjustTextareaHeight = (textarea) => {
-    textarea.style.height = "auto";
-    textarea.style.height = textarea.scrollHeight + "px";
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -38,68 +23,74 @@ export default function Contact() {
       alert("Invalid email address");
       return;
     }
-    console.log();
+
+    // Now, you can submit the form data to Netlify or perform any other actions
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
   };
 
   return (
-      <form
-        className="box"
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <h3 id="contact">Contact</h3>
-        <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="form4Example1">
-            <h4 className="field-titles">Name:</h4>
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="form4Example1"
-            className="form-control"
-          />
-        </div>
+    <form
+      className="box"
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      onSubmit={handleSubmit}
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <h3 id="contact">Contact</h3>
 
-        <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="form4Example2">
-            <h4 className="field-titles">Email Address:</h4>
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="form4Example2"
-            className={`form-control ${!isValidEmail ? "is-invalid" : ""}`}
-            value={email}
-            onChange={handleEmailChange}
-          />
-          {!isValidEmail && (
-            <div className="invalid-feedback">
-              Please enter a valid email address.
-            </div>
-          )}
-        </div>
+      <div className="form-outline mb-4">
+        <label className="form-label" htmlFor="name">
+          <h4 className="field-titles">Name:</h4>
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
 
-        <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="form4Example3">
-            <h4 className="field-titles">Message:</h4>
-          </label>
-          <textarea
-            name="message"
-            className="form-control"
-            id="form4Example3"
-            rows="4"
-            ref={messageRef}
-            value={message}
-            onChange={handleMessageChange}
-          ></textarea>
-        </div>
+      <div className="form-outline mb-4">
+        <label className="form-label" htmlFor="email">
+          <h4 className="field-titles">Email Address:</h4>
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className={`form-control ${!isValidEmail ? "is-invalid" : ""}`}
+          value={email}
+          onChange={handleEmailChange}
+        />
+        {!isValidEmail && (
+          <div className="invalid-feedback">
+            Please enter a valid email address.
+          </div>
+        )}
+      </div>
 
-        <button type="submit" className="btn btn-primary btn-block mb-4">
-          <h3 id="send">Send</h3>
-        </button>
-      </form>
+      <div className="form-outline mb-4">
+        <label className="form-label" htmlFor="message">
+          <h4 className="field-titles">Message:</h4>
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          className="form-control"
+          rows="4"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+
+      <button type="submit" className="btn btn-primary btn-block mb-4">
+        <h3 id="send">Send</h3>
+      </button>
+    </form>
   );
 }
