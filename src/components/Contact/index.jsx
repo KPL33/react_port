bb8f897b1a99f3c59e710bed3dc6b62aa5a32b3b
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-02-20 16:48:26 -0800

    further styling contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -111,24 +111,24 @@ export default function Contact2() {
   return (
     <form name="contact" method="post" className="box contact-box">
       <input type="hidden" name="form-name" value="contact" />
-      <p>
+      
         <label>
           Your Name: <input type="text" name="name" />
         </label>
-      </p>
-      <p>
+      
+      
         <label>
           Your Email: <input type="email" name="email" />
         </label>
-      </p>
-      <p>
+      
+      
         <label>
           Message: <textarea name="message"></textarea>
         </label>
-      </p>
-      <p>
+      
+      
         <button type="submit">Send</button>
-      </p>
+      
     </form>
   );
 }
