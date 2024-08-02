107891050895c764d46eaa437c1eec00f3091bc4
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-08-02 14:36:43 -0700

    Change & sign color in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,14 +5,16 @@ import './styles.css';
 //This shows the contents of our "Header". It renders the "nav-bar" component.
 export default function Header() {
     return (
-        <header className="box">
-            <div className="header-box" id="name-webdev">
-                <h1 id="name">Kevin Lewis</h1>
-                <h3 id="webdev">Graphic Designer & Full-Stack Web Developer</h3>
-            </div>
-            <div id="nav">
-                <NavBar />
-            </div>
-        </header>
+      <header className="box">
+        <div className="header-box" id="name-webdev">
+          <h1 id="name">Kevin Lewis</h1>
+          <h3 id="webdev">
+            Graphic Designer <span className="header-span">&</span> Full-Stack Web Developer
+          </h3>
+        </div>
+        <div id="nav">
+          <NavBar />
+        </div>
+      </header>
     );
 }
